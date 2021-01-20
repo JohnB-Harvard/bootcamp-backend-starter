edd320039b778df7b841339938a62667f6adf856
Alex Estevez <aestevez@college.harvard.edu>
2021-01-20 13:03:41 -0500

    fixed userById bug

--- src/graphql/Query/Users.js
+++ src/graphql/Query/Users.js
@@ -1,9 +1,9 @@
 const Course = require('../../models/Course')
 const User = require('../../models/User')
 
-const userById = async (obj, { id }) => {
+const userById = async (obj, { userID }) => {
   try {
-    const user = await User.query().findOne('id', id)
+    const user = await User.query().findOne('id', userID)
     return user
   } catch (err) {
     throw new Error('failed to get user')
